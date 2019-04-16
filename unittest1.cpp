d5801cdc8539a447aec43373f585dc1fee9919c6
secondear <49646470+secondear@users.noreply.github.com>
2019-04-16 21:55:16 +0800

    Add files via upload

---
+++ unittest1.cpp
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+extern int  MaxsumUlt(int* arr, int subs);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			int* array = {};
+			int subs = sizeof(array) / sizeof(array[0]);
+			int MAX = MaxsumUlt(array, subs);
+			Assert::AreEqual(MAX, 0);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			int array[] = { -2,11,-4,13,-5,-2 };
+			int subs = sizeof(array) / sizeof(array[0]);
+			int MAX = MaxsumUlt(array, subs);
+			Assert::AreEqual(MAX, 20);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			int array[] = { -2,-11,-4,-13,-5,-2 };
+			int subs = sizeof(array) / sizeof(array[0]);
+			int MAX = MaxsumUlt(array, subs);
+			Assert::AreEqual(MAX, 0);
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			int array[] = { 1,4,7 };
+			int subs = sizeof(array) / sizeof(array[0]);
+			int MAX = MaxsumUlt(array, subs);
+			Assert::AreEqual(MAX, 12);
+		}
+
+
+	};
+}
